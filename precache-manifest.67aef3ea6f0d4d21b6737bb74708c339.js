b42c2fdbf74744fb138e1716be91522712d30653
JuhaLuukkonen <luukkonenjuha3@gmail.com>
2020-04-21 05:38:00 +0300

    Updates

---
+++ precache-manifest.67aef3ea6f0d4d21b6737bb74708c339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c341961d1fd7107574060195b0a1a9",
+    "url": "/Huuto.netAPI/index.html"
+  },
+  {
+    "revision": "fd3dd9822ca5cfec04b8",
+    "url": "/Huuto.netAPI/static/css/main.56b22779.chunk.css"
+  },
+  {
+    "revision": "f5d4f74edf171b208d8c",
+    "url": "/Huuto.netAPI/static/js/2.6df93186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Huuto.netAPI/static/js/2.6df93186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3dd9822ca5cfec04b8",
+    "url": "/Huuto.netAPI/static/js/main.c6831010.chunk.js"
+  },
+  {
+    "revision": "84eb5cfb23a37b81b070",
+    "url": "/Huuto.netAPI/static/js/runtime-main.ebce91d5.js"
+  },
+  {
+    "revision": "b63f449e3b3519d2c99f8bfdf80ac65f",
+    "url": "/Huuto.netAPI/static/media/0käsi.b63f449e.jpg"
+  }
+]);
